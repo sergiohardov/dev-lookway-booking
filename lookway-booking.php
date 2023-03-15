c1bd5ad5add69316ed6124dc44ff062d7dde1c2c
Sergio Hardov <sergiohardov@gmail.com>
2023-03-15 23:11:40 +0200

    Plugin Starter

---
+++ lookway-booking.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Lookway Booking
+ * Description: Plugin for booking.
+ * Version: 1.0
+ * Author: Sergio Hardov
+ * Author URI: https://t.me/sergiohardov
+ * License: GPLv2 or later
+ * Text Domain: lookway-booking
+ */
