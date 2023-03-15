3c73c274b8cf2aef6dbe0c7e12e4ecb268e77811
Sergio Hardov <sergiohardov@gmail.com>
2023-03-16 01:20:59 +0200

    Create function add meta boxes, and create function for save meta boxes

--- inc/cpt.php
+++ inc/cpt.php
@@ -7,6 +7,120 @@ if (!class_exists('LookwayBookingCpt')) {
         public function register()
         {
             add_action('init', [$this, 'custom_post_type']);
+
+            add_action('add_meta_boxes', [$this, 'add_meta_box_property']);
+
+            add_action('save_post', [$this, 'save_metabox'], 10, 2);
+        }
+
+        public function add_meta_box_property()
+        {
+            add_meta_box('lookway_booking_settings', 'Property Settings', [$this, 'metabox_property_html'], 'property', 'normal', 'default');
+        }
+
+
+        public function save_metabox($post_id, $post)
+        {
+
+            if (!isset($_POST['_lookway_booking']) || !wp_verify_nonce($_POST['_lookway_booking'], 'lookway_booking_fields')) {
+                return $post_id;
+            }
+
+            if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+                return $post_id;
+            }
+
+            if ($post->post_type != 'property') {
+                return $post_id;
+            }
+
+            $post_type = get_post_type_object($post->post_type);
+            if (!current_user_can($post_type->cap->edit_post, $post_id)) {
+                return $post_id;
+            }
+
+            if (is_null($_POST['lookway_booking_price'])) {
+                delete_post_meta($post_id, 'lookway_booking_price');
+            } else {
+                update_post_meta($post_id, 'lookway_booking_price', sanitize_text_field(intval($_POST['lookway_booking_price'])));
+            }
+
+            if (is_null($_POST['lookway_booking_period'])) {
+                delete_post_meta($post_id, 'lookway_booking_period');
+            } else {
+                update_post_meta($post_id, 'lookway_booking_period', sanitize_text_field($_POST['lookway_booking_period']));
+            }
+
+            if (is_null($_POST['lookway_booking_type'])) {
+                delete_post_meta($post_id, 'lookway_booking_type');
+            } else {
+                update_post_meta($post_id, 'lookway_booking_type', sanitize_text_field($_POST['lookway_booking_type']));
+            }
+
+            if (is_null($_POST['lookway_booking_agent'])) {
+                delete_post_meta($post_id, 'lookway_booking_agent');
+            } else {
+                update_post_meta($post_id, 'lookway_booking_agent', sanitize_text_field($_POST['lookway_booking_agent']));
+            }
+
+            return $post_id;
+        }
+
+        public function metabox_property_html($post)
+        {
+            $price = get_post_meta($post->ID, 'lookway_booking_price', true);
+            $period = get_post_meta($post->ID, 'lookway_booking_period', true);
+            $type = get_post_meta($post->ID, 'lookway_booking_type', true);
+            $agent_meta = get_post_meta($post->ID, 'lookway_booking_agent', true);
+
+            wp_nonce_field('lookway_booking_fields', '_lookway_booking');
+
+
+            echo '
+            <p>
+                <label for="lookway_booking_price">Price</label>
+                <input type="number" id="lookway_booking_price" name="lookway_booking_price" value="' . esc_html($price) . '">
+            </p>
+
+            <p>
+                <label for="lookway_booking_period">Period</label>
+                <input type="text" id="lookway_booking_period" name="lookway_booking_period" value="' . esc_html($period) . '">
+            </p>
+
+            <p>
+                <label for="lookway_booking_type">Type</label>
+                <select id="lookway_booking_type" name="lookway_booking_type">
+                    <option value="">Select Type</option>
+                    <option value="sale"' . selected('sale', $type, false) . '>For Sale</option>
+                    <option value="rent"' . selected('rent', $type, false) . '>For Rent</option>
+                    <option value="sold"' . selected('sold', $type, false) . '>Sold</option>
+                </select>
+            </p>
+
+            ';
+
+            $agents = get_posts([
+                'post_type' => 'agent',
+                'numberposts' => -1
+            ]);
+
+            if ($agents) {
+                echo '
+                <p>
+                <label for="lookway_booking_agent">Agent</label>
+                <select id="lookway_booking_agent" name="lookway_booking_agent">
+                <option value="">Select Agent</option>
+                ';
+
+
+                foreach ($agents as $agent) { ?>
+                    <option value="<?php echo esc_html($agent->ID); ?>" <?php if ($agent->ID == $agent_meta) {
+                                                                            echo 'selected';
+                                                                        } ?>><?php echo esc_html($agent->post_title); ?></option>
+<?php }
+
+                echo '</select></p>';
+            }
         }
 
         public function custom_post_type()
